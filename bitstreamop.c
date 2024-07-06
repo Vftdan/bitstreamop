f4a5ca5843987cabc138b7db9e7c08d87ace2891
Vftdan <vftdan@gmail.com>
2024-07-06 17:40:59 +0200

    Basic command-line support

--- bitstreamop.c
+++ bitstreamop.c
@@ -9,12 +9,9 @@
 #include "expression.h"
 #include "parser.h"
 
-int
-main(int argc, char ** argv)
+void
+run_program(const ExprNode * program)
 {
-	(void) argc;
-	(void) argv;
-
 	BitIO io_in = file_to_bit_io(stdin, 16, 0);
 	BitIO io_out = file_to_bit_io(stdout, 0, 16);
 
@@ -24,15 +21,52 @@ main(int argc, char ** argv)
 		.scope = {NULL, NULL},
 	};
 
-	char code[] = "while (not(readeof())) (r = mul(read(1), 255); g = mul(read(1), 255); b = mul(read(1), 255); write(width(8, r)); write(width(8, g)); write(width(8, b));)";
-	Parser * parser = parser_new();
-	parser_feed(parser, code, sizeof(code) - 1);
-	const ExprNode * parsed_program = parser_end(parser);
-	evaluate_expression(&ctx, parsed_program);
-	parser_delete(parser);
+	evaluate_expression(&ctx, program);
 	bit_io_flush(&io_out);
 	free_bit_io(io_in);
 	free_bit_io(io_out);
+}
+
+void
+dump_ast(const ExprNode * program)
+{
+	FileTreePrinter printer;
+	init_file_tree_printer(&printer, stdout);
+	print_expression(&printer.as_tree_printer, program);
+}
+
+enum main_action {
+	MAINACT_RUN,
+	MAINACT_DUMP,
+};
+
+int
+main(int argc, char ** argv)
+{
+	char * code = NULL;
+	enum main_action main_action = MAINACT_RUN;
+	if (argc < 2 || !strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")) {
+		fprintf(stderr, "Usage: %s [-d] <code>\n", argc ? argv[0] : "bitstreamop");
+		return 1;
+	}
+	code = argv[1];
+	if (argc > 2 && (!strcmp(argv[1], "-d") || !strcmp(argv[1], "--dump"))) {
+		main_action = MAINACT_DUMP;
+		code = argv[2];
+	}
+
+	Parser * parser = parser_new();
+	parser_feed(parser, code, strlen(code));
+	const ExprNode * parsed_program = parser_end(parser);
+	switch (main_action) {
+	case MAINACT_RUN:
+		run_program(parsed_program);
+		break;
+	case MAINACT_DUMP:
+		dump_ast(parsed_program);
+		break;
+	}
+	parser_delete(parser);
 
 	return 0;
 }
